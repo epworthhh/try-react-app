32246b0c01aa20bb8ec815c6ec5cbb50fd0f5cfb
Héctor Figueroa <hectorfigue@MacBook-Pro-de-Hector.local>
2017-07-21 18:54:50 +0200

    Increase button & todo list

--- src/App.js
+++ src/App.js
@@ -6,19 +6,50 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {counter: 0};
+
+    this.increaseMore = this.increaseMore.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      counter: 0,
+      text: '',
+      items: []
+    };
+  }
+
+  increaseMore() {
+    this.setState({counter: this.state.counter + 1});
   }
 
   increaseCounter() {
-    setInterval(() => this.setState({counter: this.state.counter + 1}), 1000);
+    setInterval(() => this.increaseMore(), 1000);
   }
 
+  // event methods
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = {
+      id: Date.now(),
+      text: this.state.text
+    };
+
+    this.setState({items: [newItem, ...this.state.items], text: ''});
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  // lifecycle hooks
   componentDidMount() {
     this.interval = this.increaseCounter();
   }
   componentWillUnmount() {
     clearInterval(this.interval);
   }
+
 
   render() {
     return (
@@ -27,9 +58,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <div className="App-intro">
           <p>Elapsed seconds counter: {this.state.counter}</p>
-        </p>
+          <button onClick={this.increaseMore}>Faster!</button>
+        </div>
+        <div className="App-body">
+          <form onSubmit={this.handleSubmit}>
+            <h3>TODO List</h3>
+            <input onChange={this.handleChange} value={this.state.text} />
+            <button>{'>> Add #' + (this.state.items.length + 1)}</button>
+          </form>
+          <ul>
+            {this.state.items.map(item => (
+              <li key={item.id}>{item.text}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
