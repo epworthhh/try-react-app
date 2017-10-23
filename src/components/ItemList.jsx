8aa1ced7767515243b76d92d0bcee0f0f6e84787
Héctor Figueroa <hectorfigueroa@eldiario.es>
2017-10-23 12:54:46 +0200

    ItemList refactor & proptypes pkg

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function ItemList(props) {
-  const item = props.item;
-  
-  return (
-    <li>
-      <span>{item.text}</span>
-      <button className="btn-close" onClick={() => props.removeItem(item)}>X</button>
-    </li>
-  );
-}
+const ItemList = ({ item, removeItem }) => (
+  <li>
+    <span>{item.text}</span>
+    <button className="btn-close" onClick={() => removeItem(item)}>X</button>
+  </li>
+);
+
+ItemList.propTypes = {
+  item: PropTypes.object.isRequired,
+  removeItem: PropTypes.func.isRequired
+};
 
 export default ItemList;
