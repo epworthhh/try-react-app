24f9c4bfd2f1ba416cfb0a952b36e1fc61649ca6
Héctor Figueroa <hectorfigueroa@eldiario.es>
2018-09-24 17:47:03 +0200

    use prevState

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ class App extends Component {
   }
 
   increaseMore() {
-    this.setState({counter: this.state.counter + 1});
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
   }
 
   increaseCounter() {
